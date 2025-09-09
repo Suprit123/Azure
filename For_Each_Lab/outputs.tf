16aec558dc3a97e8e49db0fcb5aea3fe18687f95
Suprit123 <suprit.chinchodikar@gmail.com>
2025-09-09 13:09:08 +0530

    FMT

--- For_Each_Lab/outputs.tf
+++ For_Each_Lab/outputs.tf
@@ -1,9 +1,9 @@
 output "vnet_foreach_id" {
   description = "id's for the vnet"
-  value = azurerm_virtual_network.vnet_foreach
+  value       = azurerm_virtual_network.vnet_foreach
 }
 
 output "subnet_foreach_id" {
   description = "ids for the subnets"
-  value = azurerm_subnet.subnet_foreach
+  value       = azurerm_subnet.subnet_foreach
 }
